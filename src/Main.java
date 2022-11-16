17c96d81260a303933183f76e2af1028a8654f3a
dara2077 <dbenyacar1@gmail.com>
2022-11-16 09:30:52 -0500

    Work from class

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <=3; i++){
+            for (int j = 1; i <= 6; i++) {
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
 }
+
